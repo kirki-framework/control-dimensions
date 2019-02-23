6ad409ae0b2e8c2497eac766035e754c0ace2562
Ari <aristath@gmail.com>
2019-02-23 12:20:00 +0200

    change assets dir

--- src/Dimensions.php
+++ src/Dimensions.php
@@ -42,7 +42,7 @@ class Dimensions extends Base {
 
 		$url = apply_filters(
 			'kirki_package_url_control_dimensions',
-			trailingslashit( Kirki::$url ) . 'packages/kirki-framework/control-dimensions/src'
+			trailingslashit( Kirki::$url ) . 'vendor/kirki-framework/control-dimensions/src'
 		);
 
 		// Enqueue the script.
